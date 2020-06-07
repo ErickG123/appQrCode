2176d421076f3d9955b64a88cd9dd383d42e3860
Erick Gabriel <erickgabrielalves@hotmail.com>
2020-06-07 03:02:34 -0300

    qrCode e APK

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { Dialogs } from '@ionic-native/dialogs/ngx';
+import { Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  qrScan: any;
+  
+  constructor(public platform:Platform, public dialog:Dialogs, public qr:QRScanner) {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      document.getElementsByTagName("body")[0].style.opacity = "1";
+      this.qrScan.unsubscribe();
+    });
+  }
+
+  StartScanning() {
+    this.qr.prepare().then((status:QRScannerStatus) => {
+      if(status.authorized) {
+        this.qr.show();
+        document.getElementsByTagName("body")[0].style.opacity = "0";
+        this.qrScan = this.qr.scan().subscribe((textFound) => {
+          document.getElementsByTagName("body")[0].style.opacity = "1";
+          this.qrScan.unsubscribe();
+          this.dialog.alert(textFound);
+
+        },(err) => {
+          this.dialog.alert(JSON.stringify(err));
+        });
+
+      } else if(status.denied) {
+
+
+      } else {
+
+      }
+    });
+  }
+
+}
